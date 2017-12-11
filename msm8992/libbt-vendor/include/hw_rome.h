9dc157ae0383444f20d2fe0f939db32d1fecb16e
yang.tian <yang.tian@nxp.com>
2017-12-11 19:53:24 +0800

    MA-10591-3 [Bluetooth]Enable 1CQ and disable 1CX bluetooth on mscale
    Add bluetooth vendor hal support for 1CQ by modifying msm8992 hal.
    Set bluetooth uart port for 1CQ.
    Set firmware path for 1CQ.
    
    Signed-off-by: Tian Yang<yang.tian@nxp.com>
    Change-Id: Ibcf2b7c70c1d67da11ecf759b8e75ecce8ef88aa

--- msm8992/libbt-vendor/include/hw_rome.h
+++ msm8992/libbt-vendor/include/hw_rome.h
@@ -108,6 +108,7 @@
 #define HCI_VS_GET_ADDON_FEATURES_EVENT      (0x1B)
 #define EDL_BOARD_ID_RESPONSE                (0x23)
 #define HCI_VS_GET_BUILD_VER_EVT             (0x05)
+#define HCI_VS_STRAY_EVT                (0x17)
 
 /* Status Codes of HCI CMD execution*/
 #define HCI_CMD_SUCCESS                     (0x0)
@@ -166,6 +167,8 @@
 #define ROME_NVM_TLV_3_0_2_PATH         "/vendor/firmware/btnv32.bin"
 //#define ROME_RAMPATCH_TLV_3_0_2_PATH    "/system/etc/firmware/rampatch_tlv_3.2.tlv"
 //#define ROME_NVM_TLV_3_0_2_PATH         "/system/etc/firmware/nvm_tlv_3.2.bin"
+#define TF_RAMPATCH_TLV_1_0_1_PATH     "/vendor/firmware/rampatch_tlv_3.2.tlv"
+#define TF_NVM_TLV_1_0_1_PATH        "/vendor/firmware/nvm_tlv_3.2.bin"
 
 #define ROME_3_1_FW_SU  "bprm.cnss.3.1"
 #define ROME_3_2_FW_SU  "btfwp.cnss.3.2"
@@ -272,6 +275,7 @@ enum{
     ROME_SOC_ID_00 = 0x00000000,
     ROME_SOC_ID_11 = 0x00000011,
     ROME_SOC_ID_22 = 0x00000022,
+    ROME_SOC_ID_23 = 0x00000023,
     ROME_SOC_ID_44 = 0x00000044
 };
 
@@ -282,6 +286,7 @@ enum{
     ROME_VER_1_3 = ((ROME_PATCH_VER_0200 << 16 ) | ROME_SOC_ID_00 ),
     ROME_VER_2_1 = ((ROME_PATCH_VER_0200 << 16 ) | ROME_SOC_ID_11 ),
     ROME_VER_3_0 = ((ROME_PATCH_VER_0300 << 16 ) | ROME_SOC_ID_22 ),
-    ROME_VER_3_2 = ((ROME_PATCH_VER_0302 << 16 ) | ROME_SOC_ID_44 )
+    ROME_VER_3_2 = ((ROME_PATCH_VER_0302 << 16 ) | ROME_SOC_ID_44 ),
+    TUFELLO_VER_1_1 = ((ROME_PATCH_VER_0302 << 16 ) | ROME_SOC_ID_23 )
 };
 #endif /* HW_ROME_H */
